f35c29d8930923d89833b92a6f99ff20c27c080f
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-03-14 16:08:34 -0400

    WIP

--- lib/search/presenters.rb
+++ lib/search/presenters.rb
@@ -50,7 +50,7 @@ module Search::Presenters
       title: datastore.title,
       current_datastore: slug,
       description: datastore.description,
-      icons: Icons.new((record.icons || []) + ["mail", "chat", "format_quote", "draft", "link"]),
+      icons: Icons.new((record.icons || []) + ["mail", "chat", "format_quote", "draft", "link", "collections_bookmark", "devices"]),
       slug: datastore.slug,
       styles: ["styles.css", "datastores/styles.css", "datastores/record/styles.css"],
       scripts: ["scripts.js", "partials/scripts.js", "datastores/record/scripts.js"],
--- lib/search/presenters/record/catalog.rb
+++ lib/search/presenters/record/catalog.rb
@@ -123,7 +123,19 @@ module Search
           def marc_record
           end
 
+          def citations
+            OpenStruct.new(
+              mla: "<i>Birds</i>. v. 1-Jan./Feb. 1966-, 1966-2013.",
+              apa: "<i>Birds</i> (No. v. 1-Jan./Feb. 1966-). (1966-2013).",
+              chicago: "\"Birds,\" 1966-2013.",
+              ieee: "\"Birds,\" Art. no. v. 1-Jan./Feb. 1966-, 1966-2013.",
+              nlm: "Birds. Sandy, Bedfordshire, Eng.: Royal Society for the Protection of Birds; 1966-2013;",
+              bibtex: "@article{Birds_1966-2013, address={Sandy, Bedfordshire, Eng.}, callNumber={QL671 .B678}, number={v. 1-Jan./Feb. 1966-}, publisher={Royal Society for the Protection of Birds}, year={1966-2013} }"
+            )
+          end
+
           def indexing_date
+            "20250217"
           end
 
           def format
