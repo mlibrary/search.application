e7bd70a2a7e5635bcc391bf0470b9923e8e9af7d
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-03-21 13:37:02 -0400

    Specs for `Copy link` tab in `Actions`.

--- assets/scripts/datastores/partials/actions/_link.js
+++ assets/scripts/datastores/partials/actions/_link.js
@@ -2,7 +2,7 @@ const copyLink = () => {
   const link = document.querySelector('.actions__form.link');
   const urlInput = link.querySelector('#action__link--input');
   const copyLinkButton = link.querySelector('.link .link__copy');
-  
+
   copyLinkButton.removeAttribute('disabled');
   copyLinkButton.addEventListener('click', (event) => {
     event.preventDefault();
