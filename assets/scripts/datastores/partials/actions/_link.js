0bacc294d597acdb083b6e61d25dec9503c77172
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-03-21 09:07:35 -0400

    Copy link scripts.

---
+++ assets/scripts/datastores/partials/actions/_link.js
@@ -0,0 +1,13 @@
+const copyLink = () => {
+  const link = document.querySelector('.actions__form.link');
+  const urlInput = link.querySelector('#action__link--input');
+  const copyLinkButton = link.querySelector('.link .link__copy');
+  
+  copyLinkButton.removeAttribute('disabled');
+  copyLinkButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    navigator.clipboard.writeText(urlInput.value);
+  });
+};
+
+export default copyLink;
