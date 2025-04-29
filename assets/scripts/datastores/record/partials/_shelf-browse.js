11a6a63473bc8f2ac6e4bbf078b65efb207c8a4e
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-04-29 11:44:52 -0400

    Calculate `data-page` attributes.

--- assets/scripts/datastores/record/partials/_shelf-browse.js
+++ assets/scripts/datastores/record/partials/_shelf-browse.js
@@ -3,7 +3,7 @@
   - [x] List item has data-page= -1/0/1
   - [x] Clicking on move(-1) will change data-current-page=-1
   - [x] All list items of data-page=-1 will display and the rest will not
-  - [] Set data-current-page=0 on resize
+  - [x] Set data-current-page=0 on resize
   - [] Apply animations on previous/next click
   - [] Change pagination button titles on resize
 */
@@ -19,13 +19,21 @@ const pageLimit = () => {
   return 1;
 };
 
-const getButtons = () => {
+const getElements = () => {
   const shelfBrowseContainer = document.querySelector('.shelf-browse');
+
+  // Buttons
   const previousButton = shelfBrowseContainer.querySelector('button.shelf-browse__carousel--button-previous');
   const nextButton = shelfBrowseContainer.querySelector('button.shelf-browse__carousel--button-next');
   const returnButton = shelfBrowseContainer.querySelector('button.shelf-browse__return');
 
+  // List
+  const list = shelfBrowseContainer.querySelector('.shelf-browse__carousel--items');
+  const items = list.querySelectorAll('li');
+
   return {
+    items,
+    list,
     nextButton,
     previousButton,
     returnButton
@@ -33,42 +41,52 @@ const getButtons = () => {
 };
 
 const setPageNumbers = () => {
-  const shelfBrowseContainer = document.querySelector('.shelf-browse');
-  const list = shelfBrowseContainer.querySelector('.shelf-browse__carousel--items');
-  const items = list.querySelectorAll('li');
-  // Default value in case the class is not found
-  let currentIndex = -1;
-  const itemCount = pageLimit();
+  const { items, list } = getElements();
 
-  // Store the index of the matching item
-  items.forEach((item, index) => {
-    if (item.classList.contains('current-record')) {
-      currentIndex = index;
-    }
+  // Find where the current record is located in the list
+  const currentRecordIndex = Array.from(items).findIndex((item) => {
+    return item.classList.contains('current-record');
   });
+  const itemCount = pageLimit();
+  const difference = Math.floor(itemCount / 2);
 
-  // Define the previous items to show on the default page
-  for (let index = currentIndex + 1; index < (currentIndex + Math.ceil(itemCount / 2)); index++) {
+  // Reset data attributes
+  items.forEach((item) => {
+    return item.removeAttribute('data-page');
+  });
+  list.setAttribute('data-current-page', 0);
+
+  // Define the items to show on the default page
+  for (let index = currentRecordIndex - difference; index <= (currentRecordIndex + difference); index += 1) {
     items[index].setAttribute('data-page', 0);
   }
 
-  // Loop through each list item starting from index 22
-  for (let index = currentIndex - Math.ceil(itemCount / 2); index >= 0; index--) {
-    // Calculate the current page number based on position
-    const currentPage = (Math.floor(index / itemCount) + 1) - itemCount;
-    items[index].setAttribute('data-page', currentPage);
+  // Loop through each previous item starting from the current record
+  let previousCount = -1;
+  let previousDataPage = -1;
+  for (let index = currentRecordIndex - (difference + 1); index >= 0; index -= 1) {
+    // Decrease page count for every number of records
+    previousCount += 1;
+    if (previousCount === itemCount) {
+      previousCount = 0;
+      previousDataPage -= 1;
+    }
+    // Set calculated page number
+    items[index].setAttribute('data-page', previousDataPage);
   }
 
-  // Define the next items to show on the default page
-  for (let index = currentIndex - Math.floor(itemCount / 2); index < currentIndex; index++) {
-    items[index].setAttribute('data-page', 0);
-  }
-
-  // Loop through each list item starting from index 22
-  for (let index = currentIndex + Math.ceil(itemCount / 2); index < items.length; index++) {
-    // Calculate the current page number based on position
-    const currentPage = (Math.floor(index / itemCount) + 1) - itemCount;
-    items[index].setAttribute('data-page', currentPage);
+  // Loop through each next item starting from the current record
+  let nextCount = -1;
+  let nextDataPage = 1;
+  for (let index = currentRecordIndex + (difference + 1); index < items.length; index += 1) {
+    // Increase page count for every number of records
+    nextCount += 1;
+    if (nextCount === itemCount) {
+      nextCount = 0;
+      nextDataPage += 1;
+    }
+    // Set calculated page number
+    items[index].setAttribute('data-page', nextDataPage);
   }
 };
 
@@ -79,9 +97,7 @@ const move = (direction) => {
   }
 
   // Get list and items
-  const shelfBrowseContainer = document.querySelector('.shelf-browse');
-  const list = shelfBrowseContainer.querySelector('.shelf-browse__carousel--items');
-  const items = list.querySelectorAll('li');
+  const { items, list } = getElements();
 
   // Set current page
   list.setAttribute('data-current-page', Number(list.getAttribute('data-current-page')) + direction);
@@ -97,7 +113,7 @@ const move = (direction) => {
   });
 
   // Toggle disabled buttons
-  const { nextButton, previousButton, returnButton } = getButtons();
+  const { nextButton, previousButton, returnButton } = getElements();
 
   // Disable return button if on starting page
   returnButton.toggleAttribute('disabled', currentPage === '0');
@@ -123,8 +139,7 @@ const shelfBrowse = () => {
   move(0);
 
   // Get buttons
-  const shelfBrowseContainer = document.querySelector('.shelf-browse');
-  const { nextButton, previousButton, returnButton } = getButtons();
+  const { list, nextButton, previousButton, returnButton } = getElements();
 
   // Previous page
   previousButton.addEventListener('click', () => {
@@ -139,8 +154,7 @@ const shelfBrowse = () => {
   // Return button
   returnButton.addEventListener('click', () => {
     // Get current page value
-    const items = shelfBrowseContainer.querySelector('.shelf-browse__carousel--items');
-    const currentPage = Number(items.getAttribute('data-current-page'));
+    const currentPage = Number(list.getAttribute('data-current-page'));
     // Convert to a positive or negative value to move to page 0
     return move(-currentPage);
   });
