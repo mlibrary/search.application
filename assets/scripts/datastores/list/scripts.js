4b44cd86de3bd1c1a9952bfb067975091757a370
Erin Sullivan <sulliv.erin@gmail.com>
2025-09-03 11:31:16 -0400

    Adding `Actions` panel partial, scripts, and styles.

--- assets/scripts/datastores/list/scripts.js
+++ assets/scripts/datastores/list/scripts.js
@@ -1,3 +1,6 @@
+import { shareForm, tabControl } from '../partials/_actions.js';
+import copyCitation from '../partials/actions/_citation.js';
+import copyLink from '../partials/actions/_link.js';
 import { deselectAll } from './partials/_deselect-all.js';
 import { removeSelected } from './partials/_remove-selected.js';
 import { selectAll } from './partials/_select-all.js';
@@ -14,3 +17,19 @@ deselectAll();
 
 // Remove selected button
 removeSelected();
+
+// Actions panel
+tabControl('.actions');
+
+// Email
+shareForm('#actions__email--tabpanel');
+
+// Text
+shareForm('#actions__text--tabpanel');
+
+// Citations
+tabControl('.citation');
+copyCitation();
+
+// Copy link
+copyLink();
