e149d594d400457d335c3dd34775e7c45c338a75
Monique Rio <mrio@umich.edu>
2025-09-23 21:08:16 +0000

    creates sms message for single record

---
+++ lib/search/sms.rb
@@ -0,0 +1,59 @@
+class Search::SMS
+  def message
+    raise NotImplementedError
+  end
+
+  def send(phone:, client: "FIXME")
+  end
+
+  class Catalog < self
+    def self.for(id)
+      record = Search::Model::Record::Catalog.for(id)
+      new(record)
+    end
+
+    def initialize(record)
+      @record = record
+    end
+
+    def message
+      case @record.holdings.physical.list.count
+      when 0
+        <<~TXT.strip
+          Title: #{title}
+          Link: #{url} 
+        TXT
+      when 1
+        <<~TXT.strip
+          Title: #{title}
+          Location: #{location}
+          Call Number: #{call_number}
+          Catalog Record: #{url} 
+        TXT
+      else
+        <<~TXT.strip
+          Title: #{title}
+          There are multiple items available; see the catalog record for a list: #{url} 
+        TXT
+      end
+    end
+
+    private
+
+    def title
+      @record.bib.title.original.text
+    end
+
+    def location
+      @record.holdings.physical.list.first.physical_location.text
+    end
+
+    def call_number
+      @record.holdings.physical.list.first.call_number
+    end
+
+    def url
+      "#{S.base_url}/catalog/record/#{@record.bib.id}"
+    end
+  end
+end
