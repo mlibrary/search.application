212ff5111c15117677403560795d7e17fda7cf7e
Erin Sullivan <sulliv.erin@gmail.com>
2025-10-13 08:17:48 -0400

    `getActiveCitationTabpanel`, `getCitationAlert`, `getCitationInput` functions and tests.

--- assets/scripts/datastores/partials/actions/citation/_tabpanel.js
+++ assets/scripts/datastores/partials/actions/citation/_tabpanel.js
@@ -1,1 +1,16 @@
-//
+const getActiveCitationTabpanel = () => {
+  const tabpanels = document.querySelectorAll('[role="tabpanel"]');
+  return Array.from(tabpanels).find((tabpanel) => {
+    return tabpanel.style.display !== 'none';
+  });
+};
+
+const getCitationAlert = () => {
+  return getActiveCitationTabpanel().querySelector('.actions__alert');
+};
+
+const getCitationInput = () => {
+  return getActiveCitationTabpanel().querySelector('.citation__input');
+};
+
+export { getActiveCitationTabpanel, getCitationAlert, getCitationInput };
