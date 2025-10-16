2c41e0af716f92a7c7195cca96693e795966b505
Erin Sullivan <sulliv.erin@gmail.com>
2025-10-16 13:17:21 -0400

    `Download RIS` button WIP.

--- assets/scripts/datastores/partials/actions/action/_ris.js
+++ assets/scripts/datastores/partials/actions/action/_ris.js
@@ -1,23 +1,71 @@
 import { getTemporaryListCitations } from './_citation.js';
+import { viewingTemporaryList } from '../../../list/layout.js';
+
+const generateRISFileName = () => {
+  // Get today's date in YYYY-MM-DD format
+  const today = new Date();
+  const formattedDate = today.toISOString().slice(0, 10);
+
+  return `MyTemporaryList-${formattedDate}.ris`;
+};
+
+const generateTemporaryListRIS = () => {
+  return [
+    'TY  - JOUR',
+    `TI  - Title`,
+    `AU  - Author`,
+    `PY  - Publish year`,
+    'ER  -'
+  ];
+};
+
+const generateRISFile = () => {
+  return new Blob([generateTemporaryListRIS().join('\n\n')], { type: 'application/x-research-info-systems' });
+};
+
+const generateRISDownloadAnchor = () => {
+  const anchor = document.createElement('a');
+  anchor.href = URL.createObjectURL(generateRISFile());
+  anchor.download = generateRISFileName();
+  anchor.click();
+  URL.revokeObjectURL(anchor.href);
+};
+
+const downloadRISFormSubmit = (event) => {
+  event.preventDefault();
+
+  return generateRISDownloadAnchor();
+};
+
+const downloadRISFile = (download = downloadRISFormSubmit) => {
+  // Only run if viewing temporary list
+  if (!viewingTemporaryList()) {
+    return;
+  }
+
+  // `download` is passed in for testing purposes
+  document.querySelector('form.action__ris').addEventListener('submit', download);
+};
 
 const downloadTemporaryListRIS = () => {
+  // Only run if viewing temporary list
+  if (!viewingTemporaryList()) {
+    return;
+  }
+
   document.querySelector('form.action__ris').addEventListener('submit', (event) => {
     event.preventDefault();
 
     // Create content
     const blob = new Blob([getTemporaryListCitations('ris').join('\n\n')], { type: 'application/x-research-info-systems' });
 
-    // Get today's date
-    const today = new Date();
-    const formattedDate = today.toISOString().slice(0, 10);
-
     // Create a link to download the content
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = `MyTemporaryList-${formattedDate}.ris`;
+    link.download = generateRISFileName();
     link.click();
     URL.revokeObjectURL(link.href);
   });
 };
 
-export default downloadTemporaryListRIS;
+export { downloadRISFile, downloadRISFormSubmit, downloadTemporaryListRIS, generateRISDownloadAnchor, generateRISFile, generateRISFileName, generateTemporaryListRIS };
