a53edbc5fcd9abdf8f9524fd57c4d848e270446a
Erin Sullivan <sulliv.erin@gmail.com>
2026-01-09 08:45:08 -0500

    Creating JS and specs.

---
+++ assets/scripts/datastores/partials/actions/action/_my-temporary-list.js
@@ -0,0 +1,6 @@
+const toggleTemporaryListItem = ({ list }) => {
+  // eslint-disable-next-line no-console
+  console.log('Toggling temporary list item with list:', list);
+};
+
+export { toggleTemporaryListItem };
