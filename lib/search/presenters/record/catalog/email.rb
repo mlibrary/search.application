0c14d42a70790fbd30616cba8636fa5bc5c368e6
Monique Rio <mrio@umich.edu>
2026-01-30 14:29:19 -0500

    email has brief metadata

--- lib/search/presenters/record/catalog/email.rb
+++ lib/search/presenters/record/catalog/email.rb
@@ -1,5 +1,10 @@
 module Search::Presenters::Record::Catalog
   class Email < Full
+    METADATA_METHODS = [
+      :main_author,
+      :published,
+      :series
+    ]
     def holdings
       EmailHoldings.new(@record)
     end
