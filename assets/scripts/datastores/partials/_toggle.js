6983f4f95ef4b200b904803af64912ec932b765c
Erin Sullivan <sulliv.erin@gmail.com>
2026-02-13 16:37:16 -0500

    `getToggleButtonInformation`, and `setToggleButtonInformation` functions and tests. Updating `toggleItems` and tests to use the new functions.

--- assets/scripts/datastores/partials/_toggle.js
+++ assets/scripts/datastores/partials/_toggle.js
@@ -4,6 +4,16 @@ const getToggleButtons = () => {
   return document.querySelectorAll(`button[${attribute}]`);
 };
 
+const getToggleButtonInformation = ({ button }) => {
+  // Get the element that will be toggled
+  const control = document.getElementById(button.getAttribute('aria-controls'));
+  // Get the number of items to display. Convert to a number, or default to 5 if NaN.
+  const count = Number(button.getAttribute(attribute)) || 5;
+  // Get all direct child elements of the control
+  const items = control.querySelectorAll(':scope > *');
+  return { count, items, length: items.length };
+};
+
 // Hide all items beyond the count
 const toggleDisplayedItems = ({ count, isExpanded, items }) => {
   items.forEach((item, index) => {
@@ -17,42 +27,48 @@ const toggleDisplayedItems = ({ count, isExpanded, items }) => {
   });
 };
 
-const toggleItems = ({ buttons = getToggleButtons(), displayItems = toggleDisplayedItems } = {}) => {
+const setToggleButtonInformation = ({ button, length }) => {
+  // Get the current expanded state
+  const isExpanded = button.getAttribute('aria-expanded') === 'true';
+  // Set the opposite expanded state
+  button.setAttribute('aria-expanded', String(!isExpanded));
+  // Replace the button text accordingly
+  const textOptions = {
+    false: 'Show fewer',
+    true: `Show all ${length}`
+  };
+  button.textContent = button.textContent.replace(textOptions[!isExpanded], textOptions[isExpanded]);
+};
+
+const toggleItems = ({ buttonInformation = getToggleButtonInformation, buttons = getToggleButtons(), displayItems = toggleDisplayedItems, setButtonInformation = setToggleButtonInformation } = {}) => {
   buttons.forEach((button) => {
-    // Convert to a number, or default to 5 if NaN
-    const count = Number(button.getAttribute(attribute)) || 5;
-    // Get the element that will be toggled, along with the items
-    const control = document.getElementById(button.getAttribute('aria-controls'));
-    // Get all direct child elements of the control
-    const items = control.querySelectorAll(':scope > *');
+    const { count, items, length } = buttonInformation({ button });
 
     // Hide the button if there are not enough items
-    if (items.length <= count) {
+    if (length <= count) {
       button.style.display = 'none';
       return;
     }
 
-    // Store the original text for toggling
-    const textHideItems = button.textContent || 'Show fewer';
-    const textShowItems = button.textContent.replace('fewer', `all ${items.length}`);
-
-    // Hide all items except the first count
+    // Hide all items beyond the count
     displayItems({ count, isExpanded: false, items });
-    button.textContent = textShowItems;
-    button.setAttribute('aria-expanded', 'false');
+    // Set initial button information
+    setButtonInformation({ button, length });
 
     // Apply toggle logic
     button.addEventListener('click', () => {
-      const isExpanded = button.getAttribute('aria-expanded') === 'true';
-      button.setAttribute('aria-expanded', !isExpanded);
-      button.textContent = isExpanded ? textShowItems : textHideItems;
-      displayItems({ count, isExpanded: !isExpanded, items });
+      // Toggle the displayed items
+      displayItems({ count, isExpanded: !(button.getAttribute('aria-expanded') === 'true'), items });
+      // Update the button information
+      setButtonInformation({ button, length });
     });
   });
 };
 
 export {
+  getToggleButtonInformation,
   getToggleButtons,
+  setToggleButtonInformation,
   toggleDisplayedItems,
   toggleItems
 };
--- test/scripts/datastores/partials/_toggle.spec.js
+++ test/scripts/datastores/partials/_toggle.spec.js
@@ -1,14 +1,17 @@
 import {
+  getToggleButtonInformation,
   getToggleButtons,
+  setToggleButtonInformation,
   toggleDisplayedItems,
   toggleItems
 } from '../../../../assets/scripts/datastores/partials/_toggle.js';
 import { expect } from 'chai';
+import sinon from 'sinon';
 
-describe('toggleItems', function () {
+describe('toggle items', function () {
   let getItems = null;
   let getButton = null;
-  const count = 3;
+  const toggleCount = 3;
   const id = 'toggle-id';
 
   beforeEach(function () {
@@ -23,7 +26,7 @@ describe('toggleItems', function () {
         <li>List item 6</li>
         <li>List item 7</li>
       </ul>
-      <button aria-expanded="true" aria-controls="${id}" data-toggle="${count}">
+      <button aria-expanded="true" aria-controls="${id}" data-toggle="${toggleCount}">
         Show fewer
       </button>
     `;
@@ -35,9 +38,6 @@ describe('toggleItems', function () {
     getButton = () => {
       return document.querySelector('button');
     };
-
-    // Call the function
-    toggleItems();
   });
 
   afterEach(function () {
@@ -45,9 +45,54 @@ describe('toggleItems', function () {
     getButton = null;
   });
 
-  describe('getToggleButtons', function () {
+  describe('getToggleButtons()', function () {
     it('should get the toggle buttons', function () {
       expect(getToggleButtons(), 'should return all buttons with `data-toggle` attribute').to.deep.equal(document.querySelectorAll('button[data-toggle]'));
+    });
+  });
+
+  describe('getToggleButtonInformation()', function () {
+    let args = null;
+
+    beforeEach(function () {
+      args = {
+        button: getButton()
+      };
+    });
+
+    afterEach(function () {
+      args = null;
+    });
+
+    it('should return an object', function () {
+      expect(getToggleButtonInformation(args), 'should return an object').to.be.an('object');
+    });
+
+    describe('count property', function () {
+      it('should return a number equal to the `data-toggle` attribute on the button', function () {
+        const { count } = getToggleButtonInformation(args);
+        expect(count, 'the count should match the `data-toggle` attribute on the button').to.equal(toggleCount);
+      });
+
+      it('should default to 5 if the `data-toggle` attribute is not a number', function () {
+        args.button.setAttribute('data-toggle', 'not-a-number');
+        const { count } = getToggleButtonInformation(args);
+        expect(count, 'the count should default to 5 if `data-toggle` is not a number').to.equal(5);
+      });
+    });
+
+    describe('items property', function () {
+      it('should return all direct children of the controlled element', function () {
+        const { items } = getToggleButtonInformation(args);
+        expect(items, 'the items should match the direct children of the controlled element').to.deep.equal(getItems());
+      });
+    });
+
+    describe('length property', function () {
+      it('should return the number of direct children of the controlled element', function () {
+        const { length } = getToggleButtonInformation(args);
+        expect(length, 'the length should match the number of direct children of the controlled element').to.equal(getItems().length);
+      });
     });
   });
 
@@ -56,7 +101,7 @@ describe('toggleItems', function () {
 
     beforeEach(function () {
       args = {
-        count,
+        count: toggleCount,
         isExpanded: false,
         items: getItems()
       };
@@ -74,7 +119,7 @@ describe('toggleItems', function () {
 
     it('should only hide the items beyond the count when `isExpanded` is false', function () {
       args.items.forEach((item, index) => {
-        if (index > (count - 1)) {
+        if (index > (toggleCount - 1)) {
           expect(item.style.display, 'the item should be hidden').to.equal('none');
         } else {
           expect(item.getAttribute('style'), 'the item should be visible').to.be.null;
@@ -97,103 +142,123 @@ describe('toggleItems', function () {
     });
   });
 
-  describe('toggleItems', function () {
-    it(`should show the button if list has more than ${count} items`, function () {
-      // Check that the list has more than three items
-      expect(getItems().length, `the list should have more than ${count} items`).to.be.greaterThan(count);
+  describe('setToggleButtonInformation()', function () {
+    let args = null;
 
-      // Check that the button is visible
-      expect(getButton().hasAttribute('style'), 'the button should be displaying').to.be.false;
+    beforeEach(function () {
+      args = {
+        button: getButton(),
+        length: getItems().length
+      };
+
+      // Check initial state of the button's `aria-expanded` attribute
+      expect(args.button.getAttribute('aria-expanded'), '`aria-expanded` should be "true" before calling `setToggleButtonInformation`').to.equal('true');
+
+      // Check initial text of the button
+      expect(args.button.textContent.trim(), 'the button text should be "Show fewer" before calling `setToggleButtonInformation`').to.equal('Show fewer');
+
+      // Call the function
+      setToggleButtonInformation(args);
     });
 
-    it(`should not show the button if list has ${count} or less items`, function () {
-      getItems().forEach((item, index) => {
-        if (index > (count - 1)) {
-          item.remove();
-        }
+    afterEach(function () {
+      args = null;
+    });
+
+    it('should toggle the `aria-expanded` attribute on the button', function () {
+      expect(args.button.getAttribute('aria-expanded'), '`aria-expanded` should be "false" after calling `setToggleButtonInformation`').to.equal('false');
+    });
+
+    it('should update the button text accordingly', function () {
+      expect(args.button.textContent.trim(), `the button text should be "Show all ${args.length}" after calling \`setToggleButtonInformation\``).to.equal(`Show all ${args.length}`);
+    });
+  });
+
+  describe('toggleItems()', function () {
+    let getToggleButtonInformationSpy = null;
+    let toggleDisplayedItemsSpy = null;
+    let setToggleButtonInformationSpy = null;
+    let args = null;
+
+    beforeEach(function () {
+      getToggleButtonInformationSpy = sinon.spy(getToggleButtonInformation);
+      toggleDisplayedItemsSpy = sinon.spy();
+      setToggleButtonInformationSpy = sinon.spy();
+
+      args = {
+        buttonInformation: getToggleButtonInformationSpy,
+        buttons: getToggleButtons(),
+        displayItems: toggleDisplayedItemsSpy,
+        setButtonInformation: setToggleButtonInformationSpy
+      };
+
+      // Call the function
+      toggleItems(args);
+    });
+
+    afterEach(function () {
+      getToggleButtonInformationSpy = null;
+      toggleDisplayedItemsSpy = null;
+      setToggleButtonInformationSpy = null;
+      args = null;
+    });
+
+    it('should call `getToggleButtonInformation` for each button with the correct arguments', function () {
+      args.buttons.forEach((button) => {
+        expect(getToggleButtonInformationSpy.calledWithExactly({ button }), '`getToggleButtonInformation` should be called with the correct arguments').to.be.true;
       });
+    });
+
+    it('should hide the button if there are not enough items', function () {
+      // Set the `data-toggle` attribute to a number larger than the number of items
+      const button = getButton();
+      button.setAttribute('data-toggle', getItems().length + 1);
 
       // Call the function again
-      toggleItems();
+      toggleItems(args);
 
-      // Check that the list has three or less items
-      expect(getItems().length, `the list should have ${count} or less items`).to.be.lessThan(count + 1);
-
-      // Check that the button is not visible
-      expect(getButton().style.display, 'the button should not be displaying').to.equal('none');
+      // Check that the button is hidden
+      expect(button.style.display, 'the button should be hidden if there are not enough items').to.equal('none');
     });
 
-    it(`should initially show the first ${count} list items`, function () {
-      // Check that the first three list items are visible
-      const visibleItems = Array.from(getItems()).slice(0, count);
-      visibleItems.forEach((item) => {
-        expect(item.style.display, 'the item should be visible').to.not.equal('none');
-      });
-
-      // Check that the remaining items are hidden
-      const hiddenItems = Array.from(getItems()).slice(count);
-      hiddenItems.forEach((item) => {
-        expect(item.style.display, 'the item should be hidden').to.equal('none');
+    it('should call `toggleDisplayedItems` for each button with the correct arguments', function () {
+      args.buttons.forEach((button) => {
+        const { count, items } = getToggleButtonInformation({ button });
+        expect(toggleDisplayedItemsSpy.calledWithExactly({ count, isExpanded: false, items }), '`toggleDisplayedItems` should be called with the correct arguments').to.be.true;
       });
     });
 
-    it('should set the count to 5 if `data-toggle` is not a number', function () {
-      // Apply HTML to the body
-      document.body.innerHTML = `
-        <ul id="${id}">
-          <li>List item 1</li>
-          <li>List item 2</li>
-          <li>List item 3</li>
-          <li>List item 4</li>
-          <li>List item 5</li>
-          <li>List item 6</li>
-          <li>List item 7</li>
-        </ul>
-        <button aria-expanded="true" aria-controls="${id}" data-toggle="not-a-number">
-          Show fewer
-        </button>
-      `;
-
-      // Call the function again
-      toggleItems();
-
-      // Check that the first five list items are visible
-      const visibleItems = Array.from(getItems()).slice(0, 5);
-      visibleItems.forEach((item) => {
-        expect(item.style.display, 'the item should be visible').to.not.equal('none');
-      });
-
-      // Check that the remaining items are hidden
-      const hiddenItems = Array.from(getItems()).slice(5);
-      hiddenItems.forEach((item) => {
-        expect(item.style.display, 'the item should be hidden').to.equal('none');
+    it('should call `setToggleButtonInformation` for each button with the correct arguments', function () {
+      args.buttons.forEach((button) => {
+        const { length } = getToggleButtonInformation({ button });
+        expect(setToggleButtonInformationSpy.calledWithExactly({ button, length }), '`setToggleButtonInformation` should be called with the correct arguments').to.be.true;
       });
     });
 
-    it('should toggle the remaining list items', function () {
-      // Check that `aria-expanded` is set to false initially
-      expect(getButton().getAttribute('aria-expanded'), 'the button should have `aria-expanded` set to `false`').to.equal('false');
+    it('should call `toggleDisplayedItems` for each button on click with the correct arguments', function () {
+      args.buttons.forEach((button) => {
+        // Reset the spy call history
+        toggleDisplayedItemsSpy.resetHistory();
 
-      // Click the button to show all items
-      getButton().click();
+        // Click the button
+        button.click();
 
-      // Check that `aria-expanded` is set to true after clicking
-      expect(getButton().getAttribute('aria-expanded'), 'the button should have `aria-expanded` set to `true` after clicking').to.equal('true');
-      // Check that all items are visible
-      expect(Array.from(getItems()).every((item) => {
-        return item.style.display !== 'none';
-      }), 'all list items should be visible after clicking the button').to.be.true;
+        const { count, items } = getToggleButtonInformation({ button });
+        expect(toggleDisplayedItemsSpy.calledWithExactly({ count, isExpanded: !(button.getAttribute('aria-expanded') === 'true'), items }), '`toggleDisplayedItems` should be called with the correct arguments on click').to.be.true;
+      });
     });
 
-    it('should toggle button text', function () {
-      // Check that the button encourages you to show all items
-      expect(getButton().textContent.trim(), `the button text should be "Show all ${getItems().length}"`).to.equal(`Show all ${getItems().length}`);
+    it('should call `setToggleButtonInformation` for each button on click with the correct arguments', function () {
+      args.buttons.forEach((button) => {
+        // Reset the spy call history
+        setToggleButtonInformationSpy.resetHistory();
 
-      // Click the button to show all items
-      getButton().click();
+        // Click the button
+        button.click();
 
-      // Check that the button text has changed
-      expect(getButton().textContent.trim(), `the button text should start with "Show fewer" after clicking`).to.equal(`Show fewer`);
+        const { length } = getToggleButtonInformation({ button });
+        expect(setToggleButtonInformationSpy.calledWithExactly({ button, length }), '`setToggleButtonInformation` should be called with the correct arguments on click').to.be.true;
+      });
     });
   });
 });
