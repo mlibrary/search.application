c0447056ffcd0c6faddeb1f16b109f09ce9ef3c6
Erin Sullivan <sulliv.erin@gmail.com>
2026-02-19 15:54:54 -0500

    `deleteSelectedRecordsTest`, and `displayRemoveSelectedAction` functions and tests.

--- assets/scripts/datastores/partials/actions/action/_remove-selected.js
+++ assets/scripts/datastores/partials/actions/action/_remove-selected.js
@@ -1,10 +1,43 @@
-import { setTemporaryList, viewingTemporaryList } from '../../../list/layout.js';
-import { filterSelectedRecords } from '../../../list/partials/list-item/_checkbox.js';
+import { filterSelectedRecords, splitCheckboxValue } from '../../../list/partials/list-item/_checkbox.js';
+import { inTemporaryList, setTemporaryList, viewingTemporaryList } from '../../../list/layout.js';
+import { toggleAddedClass } from './_add-selected.js';
+import { toggleTabDisplay } from '../../_actions.js';
 
 const removeSelectedClass = 'actions__remove-selected';
 
 const getRemoveSelectedButton = () => {
   return document.querySelector(`#${removeSelectedClass}--tabpanel .action__remove-selected`);
+};
+
+const deleteSelectedRecordsTest = ({ checkboxValues = filterSelectedRecords(), list, removeClass = toggleAddedClass, splitValue = splitCheckboxValue }) => {
+  // Create a shallow copy of the list to update
+  const updatedList = { ...list };
+
+  // Loop through the selected record values
+  checkboxValues.forEach((value) => {
+    // Get the record's datastore and ID
+    const { recordDatastore, recordId } = splitValue({ value });
+
+    // Delete the record from the datastore in the final list
+    delete updatedList[recordDatastore][recordId];
+
+    // Remove the class to visually indicate the record is no longer in the temporary list
+    removeClass({ isAdded: false, recordDatastore, recordId });
+  });
+
+  // Return the updated list
+  return updatedList;
+};
+
+const displayRemoveSelectedAction = ({ checkedValues = filterSelectedRecords(), inList = inTemporaryList, list, splitValue = splitCheckboxValue, toggleTab = toggleTabDisplay } = {}) => {
+  // Check if all of the selected records are in the temporary list
+  const showTab = checkedValues.every((value) => {
+    const { recordDatastore, recordId } = splitValue({ value });
+    return inList({ list, recordDatastore, recordId });
+  });
+
+  // Show `Remove selected` if all of the selected records are in the temporary list
+  toggleTab({ id: removeSelectedClass, show: showTab });
 };
 
 const deleteSelectedRecords = ({ list, setList = setTemporaryList }) => {
@@ -38,6 +71,8 @@ const removeSelected = ({ list, removeAction = removeSelectedAction } = {}) => {
 
 export {
   deleteSelectedRecords,
+  deleteSelectedRecordsTest,
+  displayRemoveSelectedAction,
   getRemoveSelectedButton,
   removeSelected,
   removeSelectedAction
--- test/scripts/datastores/partials/actions/action/_remove-selected.spec.js
+++ test/scripts/datastores/partials/actions/action/_remove-selected.spec.js
@@ -1,12 +1,14 @@
 import {
   deleteSelectedRecords,
+  deleteSelectedRecordsTest,
+  displayRemoveSelectedAction,
   getRemoveSelectedButton,
   removeSelected,
   removeSelectedAction
 } from '../../../../../../assets/scripts/datastores/partials/actions/action/_remove-selected.js';
+import { filterSelectedRecords, splitCheckboxValue } from '../../../../../../assets/scripts/datastores/list/partials/list-item/_checkbox.js';
 import { getTemporaryList, nonEmptyDatastores, viewingTemporaryList } from '../../../../../../assets/scripts/datastores/list/layout.js';
 import { expect } from 'chai';
-import { filterSelectedRecords } from '../../../../../../assets/scripts/datastores/list/partials/list-item/_checkbox.js';
 import { JSDOM } from 'jsdom';
 import sinon from 'sinon';
 
@@ -46,6 +48,142 @@ describe('remove selected', function () {
   describe('getRemoveSelectedButton()', function () {
     it('should return the remove selected button element', function () {
       expect(getRemoveSelectedButton()).to.deep.equal(document.querySelector('#actions__remove-selected--tabpanel button.action__remove-selected'));
+    });
+  });
+
+  describe('deleteSelectedRecordsTest()', function () {
+    let toggleAddedClassSpy = null;
+    let splitCheckboxValueStub = null;
+    let args = null;
+
+    beforeEach(function () {
+      toggleAddedClassSpy = sinon.spy();
+      splitCheckboxValueStub = sinon.stub();
+      splitCheckboxValueStub.callsFake(({ value }) => {
+        return splitCheckboxValue({ value });
+      });
+      args = {
+        checkboxValues: filterSelectedRecords(),
+        list: global.temporaryList,
+        removeClass: toggleAddedClassSpy,
+        splitValue: splitCheckboxValueStub
+      };
+
+      // Check that there are selected records to test
+      expect(args.checkboxValues.length, 'there should be selected records to test').to.be.greaterThan(0);
+    });
+
+    afterEach(function () {
+      toggleAddedClassSpy = null;
+      splitCheckboxValueStub = null;
+      args = null;
+    });
+
+    it('should call `splitCheckboxValue` function with the correct arguments for each selected record', function () {
+      // Call the function
+      deleteSelectedRecordsTest({ ...args, splitValue: splitCheckboxValueStub });
+
+      // Check that the spy was called with the correct arguments for each selected record
+      args.checkboxValues.forEach((value) => {
+        expect(splitCheckboxValueStub.calledWithExactly({ value }), `splitCheckboxValue should be called with the correct arguments for record value: ${value}`).to.be.true;
+      });
+    });
+
+    it('should call `toggleAddedClass` function with the correct arguments for each selected record', function () {
+      // Call the function
+      deleteSelectedRecordsTest(args);
+
+      // Check that the spy was called with the correct arguments for each selected record
+      args.checkboxValues.forEach((value) => {
+        // Get the record's datastore and ID
+        const { recordDatastore, recordId } = splitCheckboxValue({ value });
+        // Checl that `toggleAddedClass` was called with the correct arguments
+        expect(toggleAddedClassSpy.calledWithExactly({ isAdded: false, recordDatastore, recordId }), `toggleAddedClass should be called with the correct arguments for record value: ${value}`).to.be.true;
+      });
+    });
+
+    it('should return an object', function () {
+      // Call the function
+      const result = deleteSelectedRecordsTest(args);
+
+      // Check that the result is an object
+      expect(result).to.be.an('object');
+    });
+
+    it('should return an updated list object with the selected records removed', function () {
+      // Call the function
+      const updatedList = deleteSelectedRecordsTest(args);
+
+      // Loop through the selected record values
+      args.checkboxValues.forEach((value) => {
+        // Get the record's datastore and ID
+        const { recordDatastore, recordId } = splitCheckboxValue({ value });
+        // Check that the updated list no longer contains the removed record
+        expect(Object.keys(updatedList[recordDatastore]).includes(recordId), 'the updated list should no longer contain the record that is checked').to.be.false;
+      });
+    });
+  });
+
+  describe('displayRemoveSelectedAction()', function () {
+    let inTemporaryListSpy = null;
+    let splitCheckboxValueStub = null;
+    let toggleTabDisplaySpy = null;
+    let args = null;
+
+    beforeEach(function () {
+      inTemporaryListSpy = sinon.spy();
+      splitCheckboxValueStub = sinon.stub().returns((checkbox) => {
+        return splitCheckboxValue({ value: checkbox.value });
+      });
+      toggleTabDisplaySpy = sinon.spy();
+
+      args = {
+        checkedValues: filterSelectedRecords(),
+        inList: inTemporaryListSpy,
+        list: global.temporaryList,
+        splitValue: splitCheckboxValue,
+        toggleTab: toggleTabDisplaySpy
+      };
+
+      // Check that there is at least one checked checkbox value to test with
+      expect(args.checkedValues.length, 'there should be at least one checked checkbox value to test with').to.be.greaterThan(0);
+
+      // Call the function
+      displayRemoveSelectedAction(args);
+    });
+
+    afterEach(function () {
+      inTemporaryListSpy = null;
+      splitCheckboxValueStub = null;
+      toggleTabDisplaySpy = null;
+      args = null;
+    });
+
+    it('should call `splitCheckboxValue` at least once with the correct arguments', function () {
+      // Call the function with the stubbed `splitValue`
+      displayRemoveSelectedAction({ ...args, splitValue: splitCheckboxValueStub });
+
+      // Check that `splitCheckboxValue` was called at least once with the correct arguments
+      expect(splitCheckboxValueStub.calledWithExactly({ value: args.checkedValues[0] }), '`splitCheckboxValue` should be called with the correct arguments').to.be.true;
+    });
+
+    it('should call `inTemporaryList` at least once with the correct arguments', function () {
+      // Get the expected arguments for the first checked checkbox value
+      const { recordDatastore, recordId } = splitCheckboxValue({ value: args.checkedValues[0] });
+
+      // Check that `inTemporaryList` was called at least once with the correct arguments
+      expect(inTemporaryListSpy.calledWithExactly({ list: args.list, recordDatastore, recordId }), '`inTemporaryList` should be called with the correct arguments').to.be.true;
+    });
+
+    it('should call `toggleTab` with the correct arguments', function () {
+      // Check if all checked values are already in the temporary list
+      const showTab = args.checkedValues.every((value) => {
+        const { recordDatastore, recordId } = splitCheckboxValue({ value });
+        return inTemporaryListSpy({ list: args.list, recordDatastore, recordId });
+      });
+
+      // Check that `toggleTab` was called with the correct arguments
+      expect(toggleTabDisplaySpy.calledWithExactly({ id: 'actions__remove-selected', show: showTab }), '`toggleTab` should be called with the correct arguments for the `remove-selected` tab').to.be.true;
     });
   });
 
