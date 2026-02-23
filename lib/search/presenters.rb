6d1bd924fc8abe1604250588d59127c29aac012a
Monique Rio <mrio@umich.edu>
2026-02-23 17:14:39 -0500

    presenters uses other way of writing class methods

--- lib/search/presenters.rb
+++ lib/search/presenters.rb
@@ -11,31 +11,33 @@ require "search/presenters/search_options"
 require "search/presenters/page"
 
 module Search::Presenters
-  def self.static_page_slugs
-    Page::StaticPage.slugs
-  end
+  class << self
+    def static_page_slugs
+      Page::StaticPage.slugs
+    end
 
-  def self.for_datastore(slug:, uri:, patron: nil)
-    Page::DatastoreStaticPage.for(slug: slug, uri: uri, patron: patron)
-  end
+    def for_static_page(slug:, uri:, patron:)
+      Page::StaticPage.for(slug: slug, uri: uri, patron: patron)
+    end
 
-  def self.for_datastore_record(slug:, uri:, patron:, record_id:)
-    Page::DatastoreRecordPage.for(slug: slug, uri: uri, patron: patron, record_id: record_id)
-  end
+    def for_404_page(uri:, patron:)
+      Page::StaticPage.for(slug: "404", uri: uri, patron: patron)
+    end
 
-  def self.for_datastore_results(slug:, uri:, patron: nil)
-    Page::DatastoreResultsPage.for(slug: slug, uri: uri, patron: patron)
-  end
+    def for_datastore(slug:, uri:, patron: nil)
+      Page::DatastoreStaticPage.for(slug: slug, uri: uri, patron: patron)
+    end
 
-  def self.for_list(slug:, uri:, patron: nil)
-    Page::List.for(uri: uri, patron: patron)
-  end
+    def for_list(slug:, uri:, patron: nil)
+      Page::List.for(uri: uri, patron: patron)
+    end
 
-  def self.for_static_page(slug:, uri:, patron:)
-    Page::StaticPage.for(slug: slug, uri: uri, patron: patron)
-  end
+    def for_datastore_record(slug:, uri:, patron:, record_id:)
+      Page::DatastoreRecordPage.for(slug: slug, uri: uri, patron: patron, record_id: record_id)
+    end
 
-  def self.for_404_page(uri:, patron:)
-    Page::StaticPage.for(slug: "404", uri: uri, patron: patron)
+    def for_datastore_results(slug:, uri:, patron: nil)
+      Page::DatastoreResultsPage.for(slug: slug, uri: uri, patron: patron)
+    end
   end
 end
