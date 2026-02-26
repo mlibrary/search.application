392d4f048a1acaa0dbf1b15f41aac7fbf198dbeb
Monique Rio <mrio@umich.edu>
2026-02-26 16:06:11 -0500

    results presenter has entries

--- lib/search/presenters/page/results.rb
+++ lib/search/presenters/page/results.rb
@@ -1,5 +1,37 @@
 class Search::Presenters::Page
   class Results < DatastoreStatic
+    FIXED_RECORD_IDS = [
+      990038939650106381,
+      990006758990106381,
+      99187579295006381,
+      990040646010106381,
+      11102699205,
+      11100320512,
+      11100323049,
+      11010551022,
+      11011665129,
+      990008570110106381,
+      990008570170106381,
+      990008570210106381,
+      990008570040106381,
+      990008839630106381,
+      99187591950506381,
+      99187482531606381,
+      99187422679306381,
+      11100372386,
+      99187272754306381,
+      99188902815106381,
+      990015014190106381,
+      990043272160106381,
+      990039822770106381,
+      990024357620106381
+    ]
+    FIXED_RECORDS = FIXED_RECORD_IDS.map do |id|
+      record_data = JSON.parse(File.read("#{S.project_root}/spec/fixtures/results/#{id}.json"))
+      Search::Presenters::Record::Catalog::Brief.new(
+        Search::Models::Record::Catalog.new(record_data)
+      )
+    end
     EXTRA_ICONS =
       ["add", "delete", "mail", "close", "chat", "format_quote", "draft",
         "link", "collections_bookmark", "devices", "keyboard_arrow_right",
@@ -45,6 +77,10 @@ class Search::Presenters::Page
 
     def clear_filters_url
       @uri.to_s
+    end
+
+    def entries
+      FIXED_RECORDS[1, 10]
     end
 
     private
