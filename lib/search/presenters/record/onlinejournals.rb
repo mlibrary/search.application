04dbf6281eebe8f82bc5a47d5f9ff969af96c6eb
Erin Sullivan <sulliv.erin@gmail.com>
2026-04-24 15:10:44 -0400

    Record Brief information.

--- lib/search/presenters/record/onlinejournals.rb
+++ lib/search/presenters/record/onlinejournals.rb
@@ -17,9 +17,29 @@ module Search
         end
 
         class Full < Search::Presenters::Record::Catalog::Full
+          def url
+            "#{S.base_url}/onlinejournals/record/#{id}"
+          end
+
+          def method_missing(method, *args, **kwargs, &block)
+            super unless respond_to_missing?(method)
+            S.logger.debug("#{method} not defined in Presenters::Record::Onlinejournals::Full")
+            nil
+          end
+
+          def shelf_browse
+            @shelf_browse ||= begin
+              result = nil
+            end
+          end
         end
 
         class Brief < Search::Presenters::Record::Catalog::Brief
+          METADATA_METHODS = [
+            :contributors,
+            :published,
+            :summary
+          ]
         end
 
         class Field < Search::Presenters::Record::Catalog::Field
