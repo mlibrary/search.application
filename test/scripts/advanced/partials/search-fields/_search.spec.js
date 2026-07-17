0349b4c9fe7d4cd534a7e70370b5e950875d321b
Erin Sullivan <sulliv.erin@gmail.com>
2026-07-17 16:00:08 -0400

    Updating unit tests for `_search.js`.

--- test/scripts/advanced/partials/search-fields/_search.spec.js
+++ test/scripts/advanced/partials/search-fields/_search.spec.js
@@ -1,5 +1,7 @@
 import {
   getSearchInput,
+  resetSearchInputValue,
+  updateSearchInput,
   updateSearchInputLabel
 } from '../../../../../assets/scripts/advanced/partials/search-fields/_search.js';
 import { expect } from 'chai';
@@ -66,6 +68,63 @@ describe('search input', function () {
 
     it('should update the `aria-label` attribute of the search input', function () {
       expect(searchInput().getAttribute('aria-label'), 'the `aria-label` attribute of the search input should be updated with the new index').to.equal(`Query input for search field ${args.index}`);
+    });
+  });
+
+  describe('resetSearchInputValue()', function () {
+    let getSearchInputStub = null;
+    let args = null;
+
+    beforeEach(function () {
+      getSearchInputStub = sinon.stub().returns(getSearchInput({ searchField: searchField() }));
+      args = {
+        searchField: searchField(),
+        searchInput: getSearchInputStub
+      };
+
+      // Call the function
+      resetSearchInputValue(args);
+    });
+
+    afterEach(function () {
+      getSearchInputStub = null;
+      args = null;
+    });
+
+    it('should call `getSearchInput` with the correct arguments', function () {
+      expect(getSearchInputStub.calledWith({ searchField: searchField() }), '`getSearchInput` should be called with the correct arguments').to.be.true;
+    });
+
+    it('should reset the value of the search input to an empty string', function () {
+      expect(searchInput().value, 'the value of the search input should be reset to an empty string').to.equal('');
+    });
+  });
+
+  describe('updateSearchInput()', function () {
+    let updateSearchInputLabelSpy = null;
+    let resetSearchInputValueSpy = null;
+    let args = null;
+
+    beforeEach(function () {
+      updateSearchInputLabelSpy = sinon.spy();
+      resetSearchInputValueSpy = sinon.spy();
+      args = {
+        index: 3,
+        resetSearchInput: resetSearchInputValueSpy,
+        searchField: searchField(),
+        updateLabel: updateSearchInputLabelSpy
+      };
+
+      // Call the function
+      updateSearchInput(args);
+    });
+
+    it('should call `updateSearchInputLabel` with the correct arguments', function () {
+      expect(updateSearchInputLabelSpy.calledWith({ index: args.index, searchField: args.searchField }), '`updateSearchInputLabel` should be called with the correct arguments').to.be.true;
+    });
+
+    it('should call `resetSearchInputValue` with the correct arguments', function () {
+      expect(resetSearchInputValueSpy.calledWith({ searchField: args.searchField }), '`resetSearchInputValue` should be called with the correct arguments').to.be.true;
     });
   });
 });
