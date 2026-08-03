5dfed1a0fa416cb4f2a7bc9255570a2a95daa248
Erin Sullivan <sulliv.erin@gmail.com>
2026-08-03 11:54:01 -0400

    Updating `_show-selected` functions and tests.

--- assets/scripts/advanced/partials/additional-options/selections/_show-selected.js
+++ assets/scripts/advanced/partials/additional-options/selections/_show-selected.js
@@ -1,3 +1,5 @@
+import { getSelectionsCheckboxesByState, toggleSelectionsUncheckedCheckboxes } from './_checkboxes.js';
+
 const getShowSelectedButton = ({ selections }) => {
   return selections.querySelector('.additional-option__selections--show-selected');
 };
@@ -7,36 +9,96 @@ const updateShowSelectedButtonCount = ({ button, count }) => {
   countElement.textContent = count;
 };
 
-const toggleShowSelectedButton = ({ button, count }) => {
+const isShowSelectedButtonPressed = ({ button }) => {
+  return button.getAttribute('aria-pressed') === 'true';
+};
+
+const toggleShowSelectedButtonPressed = ({ button, isPressed = isShowSelectedButtonPressed({ button }) }) => {
+  // Set `aria-pressed` to the opposite of the current pressed state
+  button.setAttribute('aria-pressed', String(!isPressed));
+};
+
+const toggleShowSelectedButtonClass = ({ button, isPressed = isShowSelectedButtonPressed({ button }) }) => {
+  button.classList.toggle('button__ghost--active', !isPressed);
+};
+
+const toggleShowSelectedButtonText = ({ button, isPressed = isShowSelectedButtonPressed({ button }) }) => {
+  const notPressedText = button.querySelector('.additional-option__selections--show-selected-not-pressed');
+  const pressedText = button.querySelector('.additional-option__selections--show-selected-pressed');
+  if (isPressed) {
+    notPressedText.setAttribute('style', 'display: none;');
+    pressedText.removeAttribute('style');
+  } else {
+    notPressedText.removeAttribute('style');
+    pressedText.setAttribute('style', 'display: none;');
+  }
+};
+
+const toggleShowSelectedButtonItems = ({
+  button,
+  toggleClass = toggleShowSelectedButtonClass,
+  togglePressed = toggleShowSelectedButtonPressed,
+  toggleText = toggleShowSelectedButtonText
+}) => {
+  // Toggle the button's active class
+  toggleClass({ button });
+  // Toggle the pressed state of the button
+  togglePressed({ button });
+  // Toggle the text of the button
+  toggleText({ button });
+};
+
+const toggleShowSelectedButtonVisibility = ({ button, getCheckedCheckboxes = getSelectionsCheckboxesByState, selections }) => {
   // Toggle the visibility of the button, if the count is greater than 0
-  if (count > 0) {
+  const checkedCheckboxes = getCheckedCheckboxes({ checked: true, selections });
+  if (checkedCheckboxes.length > 0) {
     button.removeAttribute('style');
   } else {
     button.setAttribute('style', 'display: none;');
   }
 };
 
-const handleShowSelectedFilters = ({ button, selections, toggleUncheckedCheckboxes }) => {
+const handleShowSelectedFilters = ({
+  button,
+  selections,
+  toggleButtonItems = toggleShowSelectedButtonItems,
+  toggleUncheckedCheckboxes = toggleSelectionsUncheckedCheckboxes
+}) => {
   // Add functionality to handle showing selected filters
   button.addEventListener('click', () => {
+    toggleButtonItems({ button });
     toggleUncheckedCheckboxes({ selections });
   });
 };
 
-const showSelectedFilters = ({ checkedCheckboxes, handleShowSelected = handleShowSelectedFilters, selections, showSelectedButton = getShowSelectedButton, toggleButton = toggleShowSelectedButton }) => {
+const initializeShowSelected = ({
+  handleShowSelected = handleShowSelectedFilters,
+  selections,
+  showSelectedButton = getShowSelectedButton,
+  toggleButtonVisibility = toggleShowSelectedButtonVisibility
+}) => {
+  // Get the show selected button
   const button = showSelectedButton({ selections });
 
+  // Save the arguments
+  const args = { button, selections };
+
   // Toggle the visibility of the show selected button on load
-  toggleButton({ button, count: checkedCheckboxes.length });
+  toggleButtonVisibility(args);
 
   // Handle show selected filters
-  handleShowSelected({ button, selections });
+  handleShowSelected(args);
 };
 
 export {
   getShowSelectedButton,
   handleShowSelectedFilters,
-  showSelectedFilters,
-  toggleShowSelectedButton,
+  initializeShowSelected,
+  isShowSelectedButtonPressed,
+  toggleShowSelectedButtonClass,
+  toggleShowSelectedButtonItems,
+  toggleShowSelectedButtonPressed,
+  toggleShowSelectedButtonText,
+  toggleShowSelectedButtonVisibility,
   updateShowSelectedButtonCount
 };
--- test/scripts/advanced/partials/additional-options/selections/_show-selected.spec.js
+++ test/scripts/advanced/partials/additional-options/selections/_show-selected.spec.js
@@ -1,11 +1,17 @@
 import {
   getShowSelectedButton,
   handleShowSelectedFilters,
-  showSelectedFilters,
-  toggleShowSelectedButton,
+  initializeShowSelected,
+  isShowSelectedButtonPressed,
+  toggleShowSelectedButtonClass,
+  toggleShowSelectedButtonItems,
+  toggleShowSelectedButtonPressed,
+  toggleShowSelectedButtonText,
+  toggleShowSelectedButtonVisibility,
   updateShowSelectedButtonCount
 } from '../../../../../../assets/scripts/advanced/partials/additional-options/selections/_show-selected.js';
 import { expect } from 'chai';
+import { getSelectionsCheckboxesByState } from '../../../../../../assets/scripts/advanced/partials/additional-options/selections/_checkboxes.js';
 import sinon from 'sinon';
 
 describe('show selected', function () {
@@ -30,8 +36,9 @@ describe('show selected', function () {
             <span>African American Studies</span>
           </label>        
         </fieldset>
-        <button class="additional-option__selections--show-selected" style="display: none;">
-          Show Selected (<span class="additional-option__selections--show-selected-count">0</span>)
+        <button class="additional-option__selections--show-selected" style="display: none;" aria-pressed="false">
+          <span class="additional-option__selections--show-selected-not-pressed">Show only selected options (<span class="additional-option__selections--show-selected-count">0</span>)</span>
+          <span class="additional-option__selections--show-selected-pressed" style="display: none;">Show all options</span>
         </button>
       </div>
     `;
@@ -78,50 +85,219 @@ describe('show selected', function () {
     });
   });
 
-  describe('toggleShowSelectedButton()', function () {
+  describe('isShowSelectedButtonPressed()', function () {
     let args = null;
 
     beforeEach(function () {
       args = {
-        button: getButton(),
-        count: 5
+        button: getButton()
       };
-
-      // Check that the button is initially hidden
-      expect(args.button.style.display, 'Button should initially be hidden').to.equal('none');
-
-      // Call the function
-      toggleShowSelectedButton(args);
     });
 
     afterEach(function () {
       args = null;
     });
 
-    it('should toggle the show selected button', function () {
-      if (args.count > 0) {
-        expect(args.button.style.display, '`toggleShowSelectedButton` should toggle the display').to.not.equal('none');
+    it('should return the correct pressed state of the button', function () {
+      expect(isShowSelectedButtonPressed({ button: args.button }), '`isShowSelectedButtonPressed` should return the correct pressed state').to.equal(args.button.getAttribute('aria-pressed') === 'true');
+    });
+  });
+
+  describe('toggleShowSelectedButtonPressed()', function () {
+    let args = null;
+
+    beforeEach(function () {
+      args = {
+        button: getButton(),
+        isPressed: isShowSelectedButtonPressed({ button: getButton() })
+      };
+
+      // Check that the `aria-pressed` attribute is initially set
+      expect(args.button.getAttribute('aria-pressed'), 'Button should initially have aria-pressed attribute').to.equal(String(args.isPressed));
+
+      // Call the function
+      toggleShowSelectedButtonPressed(args);
+    });
+
+    afterEach(function () {
+      args = null;
+    });
+
+    it('should toggle the pressed state of the button', function () {
+      expect(args.button.getAttribute('aria-pressed'), '`toggleShowSelectedButtonPressed` should toggle the pressed state').to.equal(String(!args.isPressed));
+    });
+  });
+
+  describe('toggleShowSelectedButtonClass()', function () {
+    let args = null;
+    const buttonClass = 'button__ghost--active';
+    let hasClass = null;
+
+    beforeEach(function () {
+      args = {
+        button: getButton(),
+        isPressed: false
+      };
+
+      hasClass = args.button.classList.contains(buttonClass);
+
+      // Check that the button has the class or not
+      expect(hasClass, `the button ${hasClass ? 'should' : 'should not'} initially have the active class`).to.be[hasClass];
+
+      // Call the function
+      toggleShowSelectedButtonClass(args);
+    });
+
+    afterEach(function () {
+      args = null;
+    });
+
+    it('should toggle the active class of the button', function () {
+      expect(args.button.classList.contains(buttonClass), '`toggleShowSelectedButtonClass` should toggle the active class').to.be[!args.isPressed];
+    });
+  });
+
+  describe('toggleShowSelectedButtonText()', function () {
+    let args = null;
+    let notPressedText = null;
+    let pressedText = null;
+
+    beforeEach(function () {
+      args = {
+        button: getButton(),
+        isPressed: false
+      };
+      notPressedText = () => {
+        return args.button.querySelector('.additional-option__selections--show-selected-not-pressed');
+      };
+      pressedText = () => {
+        return args.button.querySelector('.additional-option__selections--show-selected-pressed');
+      };
+
+      // Call the function
+      toggleShowSelectedButtonText(args);
+    });
+
+    afterEach(function () {
+      args = null;
+    });
+
+    it('should toggle the button text', function () {
+      // Check that the appropriate content is visible
+      if (args.isPressed) {
+        expect(notPressedText().hasAttribute('style'), 'Not pressed text should be visible').to.be.false;
+        expect(pressedText().getAttribute('style'), 'Pressed text should not be visible').to.equal('display: none;');
       } else {
-        expect(args.button.style.display, '`toggleShowSelectedButton` should hide the button when count is 0').to.equal('none');
+        expect(notPressedText().hasAttribute('style'), 'Not pressed text should be visible').to.be.false;
+        expect(pressedText().getAttribute('style'), 'Pressed text should not be visible').to.equal('display: none;');
+      }
+    });
+  });
+
+  describe('toggleShowSelectedButtonItems()', function () {
+    let toggleShowSelectedButtonClassSpy = null;
+    let toggleShowSelectedButtonPressedSpy = null;
+    let toggleShowSelectedButtonTextSpy = null;
+    let args = null;
+
+    beforeEach(function () {
+      toggleShowSelectedButtonClassSpy = sinon.spy();
+      toggleShowSelectedButtonPressedSpy = sinon.spy();
+      toggleShowSelectedButtonTextSpy = sinon.spy();
+      args = {
+        button: getButton(),
+        toggleClass: toggleShowSelectedButtonClassSpy,
+        togglePressed: toggleShowSelectedButtonPressedSpy,
+        toggleText: toggleShowSelectedButtonTextSpy
+      };
+
+      // Call the function
+      toggleShowSelectedButtonItems(args);
+    });
+
+    afterEach(function () {
+      toggleShowSelectedButtonClassSpy = null;
+      toggleShowSelectedButtonPressedSpy = null;
+      toggleShowSelectedButtonTextSpy = null;
+      args = null;
+    });
+
+    it('should call `toggleShowSelectedButtonClass` with the correct arguments', function () {
+      expect(toggleShowSelectedButtonClassSpy.calledOnceWithExactly({ button: args.button }), '`toggleShowSelectedButtonClass` should have been called once').to.be.true;
+    });
+
+    it('should call `toggleShowSelectedButtonPressed` with the correct arguments', function () {
+      expect(toggleShowSelectedButtonPressedSpy.calledOnceWithExactly({ button: args.button }), '`toggleShowSelectedButtonPressed` should have been called once').to.be.true;
+    });
+
+    it('should call `toggleShowSelectedButtonText` with the correct arguments', function () {
+      expect(toggleShowSelectedButtonTextSpy.calledOnceWithExactly({ button: args.button }), '`toggleShowSelectedButtonText` should have been called once').to.be.true;
+    });
+  });
+
+  describe('toggleShowSelectedButtonVisibility()', function () {
+    let getSelectionsCheckboxesByStateStub = null;
+    let args = null;
+
+    beforeEach(function () {
+      getSelectionsCheckboxesByStateStub = sinon.stub().callsFake(({ checked, selections }) => {
+        return getSelectionsCheckboxesByState({ checked, selections });
+      });
+      args = {
+        button: getButton(),
+        getCheckedCheckboxes: getSelectionsCheckboxesByStateStub,
+        selections: getSelections()
+      };
+
+      // Check the button is initially hidden
+      expect(args.button.style.display, 'Button should initially be hidden').to.equal('none');
+
+      // Call the function
+      toggleShowSelectedButtonVisibility(args);
+    });
+
+    afterEach(function () {
+      getSelectionsCheckboxesByStateStub = null;
+      args = null;
+    });
+
+    it('should call `getSelectionsCheckboxesByState` with the correct arguments', function () {
+      expect(getSelectionsCheckboxesByStateStub.calledOnceWithExactly({ checked: true, selections: args.selections }), '`getSelectionsCheckboxesByState` should have been called once').to.be.true;
+    });
+
+    it('should toggle the show selected button', function () {
+      // Check that the button's visibility is toggled based on the count of checked checkboxes
+      if (getSelectionsCheckboxesByStateStub({ checked: true, selections: args.selections }).length > 0) {
+        expect(args.button.hasAttribute('style'), '`toggleShowSelectedButtonVisibility` should toggle the display').to.be.false;
+      } else {
+        expect(args.button.style.display, '`toggleShowSelectedButtonVisibility` should hide the button when count is 0').to.equal('none');
       }
     });
   });
 
   describe('handleShowSelectedFilters()', function () {
+    let toggleShowSelectedButtonItemsSpy = null;
     let toggleUncheckedCheckboxesSpy = null;
     let args = null;
     let event = null;
 
     beforeEach(function () {
+      toggleShowSelectedButtonItemsSpy = sinon.spy();
       toggleUncheckedCheckboxesSpy = sinon.spy();
       args = {
         button: getButton(),
         selections: getSelections(),
+        toggleButtonItems: toggleShowSelectedButtonItemsSpy,
         toggleUncheckedCheckboxes: toggleUncheckedCheckboxesSpy
       };
 
       // Call the function
       handleShowSelectedFilters(args);
+
+      // Check that `toggleShowSelectedButtonItems` was not called before clicking
+      expect(toggleShowSelectedButtonItemsSpy.called, '`toggleShowSelectedButtonItems` should not have been called before clicking').to.be.false;
+      // Check that `toggleUncheckedCheckboxes` was not called before clicking
+      expect(toggleUncheckedCheckboxesSpy.called, '`toggleUncheckedCheckboxes` should not have been called before clicking').to.be.false;
 
       // Simulate a click event on the button
       event = new window.Event('click');
@@ -134,51 +310,54 @@ describe('show selected', function () {
       event = null;
     });
 
+    it('should call `toggleShowSelectedButtonItems` with the correct arguments when the button is clicked', function () {
+      expect(toggleShowSelectedButtonItemsSpy.calledOnceWithExactly({ button: args.button }), '`toggleShowSelectedButtonItems` should have been called with the correct arguments').to.be.true;
+    });
+
     it('should call `toggleUncheckedCheckboxes` with the correct arguments when the button is clicked', function () {
       expect(toggleUncheckedCheckboxesSpy.calledOnceWithExactly({ selections: args.selections }), '`toggleUncheckedCheckboxes` should have been called with the correct arguments').to.be.true;
     });
   });
 
-  describe('showSelectedFilters()', function () {
+  describe('initializeShowSelected()', function () {
     let getShowSelectedButtonStub = null;
-    let toggleShowSelectedButtonSpy = null;
     let handleShowSelectedFiltersSpy = null;
+    let toggleShowSelectedButtonVisibilitySpy = null;
     let args = null;
 
     beforeEach(function () {
-      getShowSelectedButtonStub = sinon.stub().returns(getButton());
-      toggleShowSelectedButtonSpy = sinon.spy();
+      getShowSelectedButtonStub = sinon.stub().callsFake(({ selections }) => {
+        return getShowSelectedButton({ selections });
+      });
+      toggleShowSelectedButtonVisibilitySpy = sinon.spy();
       handleShowSelectedFiltersSpy = sinon.spy();
-
       args = {
-        checkedCheckboxes: Array.from(getSelections().querySelectorAll('input[type="checkbox"]:checked')),
         handleShowSelected: handleShowSelectedFiltersSpy,
         selections: getSelections(),
         showSelectedButton: getShowSelectedButtonStub,
-        toggleButton: toggleShowSelectedButtonSpy
+        toggleButtonVisibility: toggleShowSelectedButtonVisibilitySpy
       };
 
       // Call the function
-      showSelectedFilters(args);
+      initializeShowSelected(args);
     });
 
     afterEach(function () {
-      getShowSelectedButtonStub = null;
-      toggleShowSelectedButtonSpy = null;
+      toggleShowSelectedButtonVisibilitySpy = null;
       handleShowSelectedFiltersSpy = null;
       args = null;
     });
 
     it('should call `getShowSelectedButton` with the correct arguments', function () {
-      expect(getShowSelectedButtonStub.calledOnceWithExactly({ selections: args.selections }), '`getShowSelectedButton` should have been called with the correct arguments').to.be.true;
+      expect(getShowSelectedButtonStub.calledOnceWithExactly({ selections: args.selections }), '`getShowSelectedButton` should have been called once').to.be.true;
     });
 
-    it('should call `toggleShowSelectedButton` with the correct arguments', function () {
-      expect(toggleShowSelectedButtonSpy.calledOnceWithExactly({ button: getShowSelectedButtonStub(), count: args.checkedCheckboxes.length }), '`toggleShowSelectedButton` should have been called with the correct arguments').to.be.true;
+    it('should call `toggleShowSelectedButtonVisibility`with the correct arguments', function () {
+      expect(toggleShowSelectedButtonVisibilitySpy.calledOnceWithExactly({ button: getShowSelectedButtonStub({ selections: args.selections }), selections: args.selections }), '`toggleShowSelectedButtonVisibility` should have been called once').to.be.true;
     });
 
-    it('should call `handleShowSelected` with the correct arguments', function () {
-      expect(handleShowSelectedFiltersSpy.calledOnceWithExactly({ button: getShowSelectedButtonStub(), selections: args.selections }), '`handleShowSelected` should have been called with the correct arguments').to.be.true;
+    it('should call `handleShowSelectedFilters` with the correct arguments', function () {
+      expect(handleShowSelectedFiltersSpy.calledOnceWithExactly({ button: getShowSelectedButtonStub({ selections: args.selections }), selections: args.selections }), '`handleShowSelectedFilters` should have been called once').to.be.true;
     });
   });
 });
